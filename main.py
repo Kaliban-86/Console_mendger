00d2f22eacd383582a9d688c207a3de243453734
koxan <koxanovskij86@mail.ru>
2022-06-26 17:43:05 +0300

    осталась только смена рабочей директории

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Основной модуль для запуска и отображения основного меню
+import os
+
 from Victory import play_victory
 from bank import bank
 from sys_func import os_print
 from make_dir_func import make_new_dir
 from remove_dir_func import remove_dir
 from copy_func import copy_file
+from file_list_func import file_list_in_dir, dir_list_in_dir
 
 while True:
     print('*****************************')
@@ -14,9 +17,9 @@ while True:
     print('1. Создать папку')  # +
     print('2. Удалить (файл/папку')  # +
     print('3. Копировать (файл/папку)')  # +
-    print('4. Просмотр содержимого рабочей директории')
-    print('5. Посмотреть только папки')
-    print('6. Посмотреть только файлы')
+    print('4. Просмотр содержимого рабочей директории')  # +
+    print('5. Посмотреть только папки')  # +
+    print('6. Посмотреть только файлы')  # +
     print('7. Посмотреть информацию об операционной системе')  # +
     print('9. Создатель программы')  # +
     print('10. Играть в викторину')  # +
@@ -34,6 +37,12 @@ while True:
         remove_dir()
     elif choice_num == 3:
         copy_file()
+    elif choice_num == 4:
+        print(os.listdir())
+    elif choice_num == 5:
+        dir_list_in_dir()
+    elif choice_num == 6:
+        file_list_in_dir()
     elif choice_num == 7:
         os_print()
     elif choice_num == 9:
